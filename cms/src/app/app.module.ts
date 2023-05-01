5e43531ccca8f57415456204423adb23c98b7cc8
PKwhiting <patten.whiting@gmail.com>
2023-04-30 21:18:11 -0600

    Week 3 with problems

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactsComponent } from './contacts.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,16 @@ import { ContactDetailComponent } from './contacts/contact-detail/contact-detail
     HeaderComponent,
     ContactsComponent,
     ContactListComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    ContactItemComponent,
+    DocumentsComponent,
+    DocumentItemComponent,
+    DocumentDetailComponent,
+    MessagesComponent,
+    MessageListComponent,
+    MessageItemComponent,
+    MessageEditComponent,
+    DocumentListComponent
   ],
   imports: [
     BrowserModule,
